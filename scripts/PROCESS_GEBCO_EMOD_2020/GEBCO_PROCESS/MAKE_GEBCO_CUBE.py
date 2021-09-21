fdc0f0c83d98fd41cd594d7e8cd4db09dd1b4b10
deazer <deazer@gmail.com>
2021-09-21 15:14:00 +0100

    Simple GEBCO conversion to Cube script with command line options for input and output

--- scripts/PROCESS_GEBCO_EMOD_2020/GEBCO_PROCESS/MAKE_GEBCO_CUBE.py
+++ scripts/PROCESS_GEBCO_EMOD_2020/GEBCO_PROCESS/MAKE_GEBCO_CUBE.py
@@ -1,25 +1,33 @@
-# Turns out the EDMONET data as is as a repeat of lat and lon iles so we remove them here
-# and output as a cube
+""" Convert Raw bathy into Cube Format
+
+We use the iris interp to later map the input bathy to AMM15.
+To do that we convert the bathy into cube format here
+
+Parameters
+----------
+IN_DIR : str
+    The path the input bathymetry
+OUT_DIR : str
+    The path write the output cube to
+
+Returns
+-------
+iris cube
+    The bathymetry in cube format
+"""
 import iris
-import iris.analysis.cartography
 import numpy as np
-
-from pylab import nan
-
-from scipy import ndimage as nd
-
-#from mask_tools import fill
-
 
 from iris.coords import DimCoord
 from iris.cube import Cube
-from iris.analysis.cartography import rotate_pole
 from netCDF4 import Dataset
 
 import argparse
 from pathlib import Path
 import sys
 
+
+# Just parses the command line arguments in path and out path
 
 parser = argparse.ArgumentParser(description='Process inputs and output file paths')
 parser.add_argument('-i','--IN_DIR',metavar='IN_DIR',  nargs=1,
@@ -39,28 +47,28 @@ print("\n     the INPUT directory for bathy as {}\n".format(  args.IN_DIR[0] ))
 if (Path(args.IN_DIR[0])).is_dir():
        print(" and the {} exists.".format (args.IN_DIR[0]))
 else:
-      sys.exit("However, {} does not exist  so we exit here".format(args.IN_DIR[0])) 
+      sys.exit("However, {} does not exist, so we exit here.".format(args.IN_DIR[0])) 
 print("\n     the OUTPUT directory for processed bathy as {}\n".format(  args.OUT_DIR[0] ))
 if (Path(args.OUT_DIR[0])).is_dir():
        print(" and the {} exists.".format (args.OUT_DIR[0]))
 else:
-      sys.exit("However, {} does not exist  so we exit here".format(args.OUT_DIR[0])) 
+      sys.exit("However, {} does not exist, so we exit here.".format(args.OUT_DIR[0])) 
 print("\n----------------------------------------------------\n")
 
 
 
-GEBCO_RAW_fp=Dataset('{}/NWS_CUT_GEBCO.nc'.format(args.IN_DIR[0]),'r')
+GEBCO_RAW_fp = Dataset('{}/NWS_CUT_GEBCO.nc'.format(args.IN_DIR[0]),'r')
 
-GEBCO_LAT=GEBCO_RAW_fp.variables['lat'][:]
-GEBCO_LON=GEBCO_RAW_fp.variables['lon'][:]
+GEBCO_LAT = GEBCO_RAW_fp.variables['lat'][:]
+GEBCO_LON = GEBCO_RAW_fp.variables['lon'][:]
 
-GEBCO_BATHY=GEBCO_RAW_fp.variables['elevation'][:]
+GEBCO_BATHY = GEBCO_RAW_fp.variables['elevation'][:]
 
 GEBCO_BATHY = GEBCO_BATHY.astype(float)
 
-EDMONET_BATHY_B=np.copy(GEBCO_BATHY)
+EDMONET_BATHY_B = np.copy(GEBCO_BATHY)
 
-# set up a Cube
+# set up the cube
 
 cs = iris.coord_systems.GeogCS(6371229)
 latitude_emodnet  = DimCoord(GEBCO_LAT, standard_name='latitude', units='degrees',coord_system=cs)
@@ -77,6 +85,8 @@ print (np.shape(GEBCO_BATHY))
 GEBCO_cube = Cube(GEBCO_BATHY, standard_name='sea_floor_depth_below_geoid',units='m',
             dim_coords_and_dims=[(latitude_emodnet, 0), (longitude_emodnet, 1)])
 
+
+# save it to file
 
 iris.save(GEBCO_cube, '{}/GEBCO_CUBE.nc'.format(args.OUT_DIR[0]))
 
