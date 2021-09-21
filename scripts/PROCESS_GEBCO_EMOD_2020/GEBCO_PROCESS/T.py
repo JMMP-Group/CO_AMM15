bf9f4ddf20c3b8cfc57d012ad82f1034e36d223d
deazer <deazer@gmail.com>
2021-09-21 14:06:48 +0100

    More cases dealt with

--- scripts/PROCESS_GEBCO_EMOD_2020/GEBCO_PROCESS/T.py
+++ scripts/PROCESS_GEBCO_EMOD_2020/GEBCO_PROCESS/T.py
@@ -2,6 +2,7 @@ import iris
 import numpy as np
 import argparse
 from pathlib import Path
+import sys
 
 
 parser = argparse.ArgumentParser(description='Process inputs and output file paths')
@@ -15,15 +16,30 @@ parser.add_argument( '-o','--OUT_DIR',metavar='OUT_DIR', nargs=1,
 args = parser.parse_args()
 if not all([args.AMM15_PATH, args.IN_DIR, args.OUT_DIR]):
     print(" Sorry All Arguments are required")
-    exit(1)
+    sys.exit("Sorry all the arguments are required") 
 
 
 print (args.AMM15_PATH[0])
 print("\n----------------------------------------------------\n")
-print( ("Thanks, you have chosen: \n \
-       AMM15 grid file as {}\n \
-       INPUT directory for bathy as {}\n \
-       and output for process bathy as {}.".format(args.AMM15_PATH[0], args.IN_DIR[0], args.OUT_DIR[0]) ) )
+print( "Thanks, you have chosen: \n ")
+print( "      AMM15 grid file as {}\n".format (args.AMM15_PATH[0]))
+if Path(args.AMM15_PATH[0]).is_file():
+       print(" and the file {} exists.".format (args.AMM15_PATH[0]))
+else:
+      sys.exit("However, {} does not exist  so we exit here".format(args.AMM15_PATH[0])) 
+
+print("\n     the INPUT directory for bathy as {}\n".format(  args.IN_DIR[0] ))
+if (Path(args.IN_DIR[0])).is_dir():
+       print(" and the {} exists.".format (args.IN_DIR[0]))
+else:
+      sys.exit("However, {} does not exist  so we exit here".format(args.IN_DIR[0])) 
+print("\n     the OUTPUT directory for processed bathy as {}\n".format(  args.OUT_DIR[0] ))
+if (Path(args.OUT_DIR[0])).is_dir():
+       print(" and the {} exists.".format (args.OUT_DIR[0]))
+else:
+      sys.exit("However, {} does not exist  so we exit here".format(args.OUT_DIR[0])) 
+
+#       and output for process bathy as {}.".format(args.AMM15_PATH[0], args.IN_DIR[0], args.OUT_DIR[0]) ) 
 print("\n----------------------------------------------------\n")
 
 AMM15_cube = iris.load(args.AMM15_PATH)[0]
