0588e34ac23feb6d5f8a25c24d0809ad5252cb75
enodea <enda.odea@metoffice.gov.uk>
2021-09-30 14:17:07 +0700

    Added flags for in/out

--- scripts/PROCESS_GEBCO_EMOD_2020/EMODNET_PROCESS/MAKE_EMODNET_CUBE.py
+++ scripts/PROCESS_GEBCO_EMOD_2020/EMODNET_PROCESS/MAKE_EMODNET_CUBE.py
@@ -1,38 +1,88 @@
-# Turns out the EDMONET data as is as a repeat of lat and lon iles so we remove them here
-# and output as a cube
-import cartopy.crs as ccrs
+""" Convert Raw bathy into Cube Format
+
+We use the iris interp to later map the input bathy to AMM15.
+To do that we convert the bathy into cube format here
+
+Note mergin the tiled  EDMONET data reults in a  repeat of lat and lon lines 
+so we remove them here and output as a cube
+
+Parameters
+----------
+IN_DIR : str
+    The path the input bathymetry
+OUT_DIR : str
+    The path write the output cube to
+
+Returns
+-------
+iris cube
+    The bathymetry in cube format
+"""
 import iris
-import iris.analysis.cartography
 import numpy as np
 
-from scipy import ndimage as nd
-
-#from mask_tools import fill
 
 
 from iris.coords import DimCoord
 from iris.cube import Cube
-from iris.analysis.cartography import rotate_pole
 from netCDF4 import Dataset
 
+import argparse
+from pathlib import Path
+import sys, platform
+
+from datetime import datetime
+import subprocess
 
 
-#EMODNET_RAW_cube=iris.load('INPUT_DATA//EMODNET_ALL_v2.nc')[0]
-# Turns out lat and lon in this are not monotonic look like there is a repeat we need to clean that up
 
-#EMODNET_RAW_fp=Dataset('INPUT_DATA//EMODNET_ALL_v2.nc','r')
-EMODNET_RAW_fp=Dataset('..//MERGE/ALLmerge.nc','r')
 
-EMODNET_LAT=EMODNET_RAW_fp.variables['lat'][:]
-EMODNET_LON=EMODNET_RAW_fp.variables['lon'][:]
+# Just parses the command line arguments in path and out path
 
-#EMODNET_BATHY=EMODNET_RAW_fp.variables['Bathymetry'][:]
-EMODNET_BATHY=EMODNET_RAW_fp.variables['elevation'][:]
-EDMONET_BATHY_B=np.copy(EMODNET_BATHY)
+parser = argparse.ArgumentParser(description='Process inputs and output file paths')
+parser.add_argument('-i','--IN_DIR',metavar='IN_DIR',  nargs=1,
+                    help='Path to source bathymertry files', required=True)
+parser.add_argument( '-o','--OUT_DIR',metavar='OUT_DIR', nargs=1,
+                    help='Path to output to ', required=True) 
+
+args = parser.parse_args()
+
+if not all([args.IN_DIR, args.OUT_DIR]):
+    print(" Sorry All Arguments are required")
+    sys.exit("Sorry all the arguments are required") 
+
+
+print("\n----------------------------------------------------\n")
+print( "Thanks, you have chosen: \n ")
+print("\n     the INPUT directory for bathy as {}\n".format(  args.IN_DIR[0] ))
+if (Path(args.IN_DIR[0])).is_dir():
+       print(" and the directory{} exists.".format (args.IN_DIR[0]))
+else:
+      sys.exit("However, {} does not exist, so we exit here.".format(args.IN_DIR[0])) 
+print("\n     the OUTPUT directory for processed bathy as {}\n".format(  args.OUT_DIR[0] ))
+if (Path(args.OUT_DIR[0])).is_dir():
+       print(" and the  directory {} exists.".format (args.OUT_DIR[0]))
+else:
+      sys.exit("However, {} does not exist, so we exit here.".format(args.OUT_DIR[0])) 
+print("\n----------------------------------------------------\n")
+
+#------------------------------------------------------------------------------
+# Get input data
+#------------------------------------------------------------------------------
+
+EMODNET_RAW_fp = Dataset('{}/ALLmerge.nc'.format(args.IN_DIR[0]),'r')
+
+EMODNET_LAT = EMODNET_RAW_fp.variables['lat'][:]
+EMODNET_LON = EMODNET_RAW_fp.variables['lon'][:]
+
+EMODNET_BATHY = EMODNET_RAW_fp.variables['elevation'][:]
+#EDMONET_BATHY_B = np.copy(EMODNET_BATHY)
+
+print ("Data Read In")
 
 #Travere lats and lons to find repeats
 for i in range(1,np.size(EMODNET_LON[:])):
-  if(EMODNET_LON[i]<=EMODNET_LON[i-1]):
+  if(EMODNET_LON[i] <= EMODNET_LON[i-1]):
        print ('Caught a repeat at :,i,EMODNET_LON[i-1],EMODNET_LON[i],EMODNET_LON[i+1],EMODNET_LON[i+2]')
        print ('Caught a repeat at :',i,EMODNET_LON[i-1],EMODNET_LON[i],EMODNET_LON[i+1],EMODNET_LON[i+2])
 EMODNET_BATHY = np.delete(EMODNET_BATHY,9482,1)
@@ -97,6 +147,7 @@ EMODNET_BATHY = np.delete(EMODNET_BATHY,56885,1)
 EMODNET_LON   = np.delete(EMODNET_LON,56885,0)
 
 
+print ("Before Repeat Check")
 
 
 #Repeat Check
@@ -149,24 +200,25 @@ for i in range(1,np.size(EMODNET_LAT[:])):
 
 
 ## set up a Cube
+print ("set up a Cube")
 
 cs = iris.coord_systems.GeogCS(6371229)
 latitude_emodnet  = DimCoord(EMODNET_LAT, standard_name='latitude', units='degrees',coord_system=cs)
 longitude_emodnet = DimCoord(EMODNET_LON, standard_name='longitude', units='degrees',coord_system=cs)
 
-print (np.shape(latitude_emodnet))
-print (np.shape(longitude_emodnet))
-print (np.shape(EMODNET_LAT))
-print (np.shape(EMODNET_LON))
-print (np.shape(EMODNET_BATHY))
 
 
 
 
 EMODNET_cube = Cube(EMODNET_BATHY, standard_name='sea_floor_depth_below_geoid',units='m',
             dim_coords_and_dims=[(latitude_emodnet, 0), (longitude_emodnet, 1)])
+EMODNET_cube.attributes[ 'History' ] = "Created by {} from branch {} of {} on {} ".format(script,branch[:],repos[:],current_time)
+EMODNET_cube.attributes[ 'Input' ] = "Allmerge.nc"
+EMODNET_cube.attributes[ 'Python version' ] = platform.python_version()
+EMODNET_cube.attributes[ 'System' ] = platform.system()
+EMODNET_cube.attributes[ 'Release' ] = platform.release()
 
 
-iris.save(EMODNET_cube, '../EMODNET_v2020_NO_REPEAT_LAT_LON.nc')
+iris.save(EMODNET_cube, '{}/EMODNET_v2020_NO_REPEAT_LAT_LON.nc'.format(args.OUT_DIR[0]))
 
 
